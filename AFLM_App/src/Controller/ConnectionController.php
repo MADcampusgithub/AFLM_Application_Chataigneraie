12fe358505509e063773fd48285151fab7b7774d
MADcampusgithub <maxime.adler@campus-la-chataigneraie.org>
2022-03-15 09:05:21 +0100

    Add img, clean app

--- AFLM_App/src/Controller/ConnectionController.php
+++ AFLM_App/src/Controller/ConnectionController.php
@@ -3,16 +3,27 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ConnectionController extends AbstractController
 {
     /**
-     * @Route("/connection", name="app_connection")
+     * @Route("/connection")
      */
-    public function Connection(): Response
+    public function Connection() : Response
     {
         return $this->render('connection.html.twig');
     }
+
+    /**
+     * @Route("/connection", name="app_connection", methods={"POST"})
+     */
+    public function PostConnection(Request $request) : Response {
+        $test = $request->request->all();
+        
+        return $this->render('entreprises.html.twig');
+    }
+    
 }
