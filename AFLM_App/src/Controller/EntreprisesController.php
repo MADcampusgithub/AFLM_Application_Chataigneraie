aded03992f1d8d8477909c19eef16a874eb68b68
afahloun <Alex@PCAlex.lachataigne-NLE.org>
2022-03-16 09:28:37 +0100

    Show entreprises

--- AFLM_App/src/Controller/EntreprisesController.php
+++ AFLM_App/src/Controller/EntreprisesController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpClient\HttpClient;
 
 class EntreprisesController extends AbstractController
 {
@@ -15,11 +16,16 @@ class EntreprisesController extends AbstractController
     public function Entreprises(Request $request) : Response {
         $login = $request->getSession()->get('login');
         $mdp = $request->getSession()->get('mdp');
-        
+        $client = HttpClient::create();
+        $response = $client->request('GET', "http://10.3.249.223:8001/api/entreprises", ['headers' => 
+                 ['Accept' => 'application/json']]);
+
+        $utilisateurs = $response->toArray();
+
         if ($login == "" && $mdp == "") {
             return new Response("vous devez vous enregister avant d'accéder au données");
         }
 
-        return $this->render('entreprises.html.twig', ['login' => $login]);
-    }
+        return $this->render('entreprises.html.twig', ['login' => $login, 'utilisateurs' => $utilisateurs]);
+    } 
 }
