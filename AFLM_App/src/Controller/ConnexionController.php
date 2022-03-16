9c82f3c6e52ee870a0f0e85e10e6f534bdd1d6a8
MADcampusgithub <maxime.adler@campus-la-chataigneraie.org>
2022-03-16 12:29:55 +0100

    fix session variable

--- AFLM_App/src/Controller/ConnexionController.php
+++ AFLM_App/src/Controller/ConnexionController.php
@@ -16,15 +16,9 @@ class ConnexionController extends AbstractController
      */
     public function Connexion(Request $request) : Response
     {
-        if ($request->getSession()->isStarted()) {
-            $request->getSession()->remove('login');
-            $request->getSession()->remove('mdp');
-            $request->getSession()->remove('admin');
-        }
-        else {
-            $session = new Session();
-            $session->start();
-        }
+        $request->getSession()->set('login', "");
+        $request->getSession()->set('mdp', "");
+        $request->getSession()->set('admin', false);
         return $this->render('connexion.html.twig');
     }
 
@@ -49,7 +43,7 @@ class ConnexionController extends AbstractController
             if ($utilisateurs[$i]['utiLogin'] == $login && $utilisateurs[$i]['utiMdp'] == $mdp) {
                 $request->getSession()->set('login', $login);
                 $request->getSession()->set('mdp', $mdp);
-                $request->getSession()->set('admin', $utilisateurs[$i]['utiMdp'] == 0 ? false : true);
+                $request->getSession()->set('admin', $utilisateurs[$i]['utiDroit'] == 0 ? false : true);
 
                 return $this->redirect("/entreprises");
             }
