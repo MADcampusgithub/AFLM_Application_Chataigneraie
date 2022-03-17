dcb5a6d97d2cd4565165d0a2ed1f1dff4624d68e
afahloun <Alex@PCAlex.lachataigne-NLE.org>
2022-03-17 12:03:46 +0100

    Affichage Professionnels

--- AFLM_App/src/Controller/ProfessionnelsController.php
+++ AFLM_App/src/Controller/ProfessionnelsController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpClient\HttpClient;
 
 class ProfessionnelsController extends AbstractController
 {
@@ -14,6 +15,18 @@ class ProfessionnelsController extends AbstractController
      */
     public function Professionnels(Request $request): Response
     {
-        return $this->render('professionnels.html.twig', ['login' => $request->getSession()->get('login')]);
+        $login = $request->getSession()->get('login');
+        $mdp = $request->getSession()->get('mdp');
+        $client = HttpClient::create();
+        $response = $client->request('GET', "http://10.3.249.223:8001/api/personnes", ['headers' => 
+        ['Accept' => 'application/json']]);
+
+        $personnes = $response->toArray(); 
+        
+        if ($login == "" && $mdp == "") {
+            return new Response("vous devez vous enregister avant d'accéder au données");
+        }
+
+        return $this->render('professionnels.html.twig', ['login' => $request->getSession()->get('login'), 'personnes' => $personnes]);
     }
 }
