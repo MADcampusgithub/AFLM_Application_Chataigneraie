74a4ae4159c10fc7647f4f0f6906cf9fcb36672b
MADcampusgithub <maxime.adler@campus-la-chataigneraie.org>
2022-03-29 14:56:48 +0200

    working CRUD for entreprises

--- AFLM_App/src/Controller/EntreprisesController.php
+++ AFLM_App/src/Controller/EntreprisesController.php
@@ -78,22 +78,65 @@ class EntreprisesController extends AbstractController
             for($j = 0; $j < count($entreprise['entSpecialite']); $j++) {
                 $entreprise['entSpecialite'][$j] = $this->GetData($this->specialites, "speLabel", $entreprise['entSpecialite'][$j]);
             }
-            return $this->render('entreprises.html.twig', ['login' => $login, 'entreprises' => $entreprises, 'entreprise' => $entreprise]);
+            return $this->render('entreprises.html.twig', ['login' => $login, 'entreprises' => $entreprises, 'entreprise' => $entreprise, 'specialites' => $this->specialites, 'pays' => $this->pays, 'villes' => $this->villes]);
         } else {
             return $this->render('entreprises.html.twig', ['login' => $login, 'entreprises' => $entreprises]);
         }
     }
     /**
-     * @Route("/entreprises/{id}", requirements = {"parametre"="\d+"}, name="suppr_entreprises", methods={"DELETE"})
+     * @Route("/entreprisesdelete/{id}", requirements = {"parametre"="\d+"}, name="suppr_entreprises")
      */
-    public function SupprEntreprises() : Response {
+    public function SupprEntreprises(Request $request, int $id) : Response {
+        $login = $request->getSession()->get('login');
+        $mdp = $request->getSession()->get('mdp');
+        $client = HttpClient::create();
+
+        if ($login == "" && $mdp == "") {
+            return new Response("vous devez vous enregister avant d'accéder au données");
+        }
+
+        $client->request(
+            'DELETE', 
+            "http://10.3.249.223:8001/api/entreprises/" . $id, [
+                'headers' => ['Accept' => 'application/json']
+            ]);
+
         return $this->redirect("/entreprises");
     }
 
     /**
-     * @Route("/entreprises/{id}", requirements = {"parametre"="\d+"}, name="edit_entreprises", methods={"PUT"})
+     * @Route("/entreprisesupdate/{id}", requirements = {"parametre"="\d+"}, name="edit_entreprises")
      */
-    public function EditEntreprises() : Response {
+    public function EditEntreprises(Request $request, int $id) : Response {
+        $login = $request->getSession()->get('login');
+        $mdp = $request->getSession()->get('mdp');
+        $client = HttpClient::create();
+
+        if ($login == "" && $mdp == "") {
+            return $this->redirect("/connexion");
+        }
+
+        $spes = array();
+        foreach($request->get("entSpes") as $spe) {
+            $spes[] = ("/api/specialites/" . $spe);
+        }
+
+        $client->request(
+            'PUT', 
+            "http://10.3.249.223:8001/api/entreprises/" . $id, [
+                'headers' => ['Accept' => 'application/json'],
+                'json' => [
+                    'entRS' => $request->get("entRS"),
+                    'entAdresse1' => $request->get("entAdr1"),
+                    'entAdresse2' => $request->get("entAdr2"),
+                    'entAdresse3' => $request->get("entAdr3"),
+                    'entCP' => $request->get("entCP"),
+                    'entSpecialite' => $spes,
+                    'entVille' => '/api/villes/' . $request->get("entVille"),
+                    'entPays' => '/api/pays/' . $request->get("entPays")
+                ]
+            ]);
+
         return $this->redirect("/entreprises");
     }
 }
